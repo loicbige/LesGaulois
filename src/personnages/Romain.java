7f2247fa8e5545edc028b012216f55d143088237
loicbige <loic.bige@univ-tlse3.fr>
2025-09-25 15:16:59 +0200

    ajout TP1

---
+++ src/personnages/Romain.java
@@ -0,0 +1,29 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parole(String text) {
+		System.out.println(prendreParole() + "\"" + text + "\"");
+	}
+
+
+	private String prendreParole() {
+		return "Le romain" + nom + " : ";
+	}
+	
+	
+	
+	
+
+}
